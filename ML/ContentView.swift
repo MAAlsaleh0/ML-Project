e60c2922197f816e10a8933be8d5bdebfaab7df8
Mohammed Alsaleh <malsalehh0@gmail.com>
2022-01-17 06:27:46 -0800

    Initial Commit

---
+++ ML/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ML
+//
+//  Created by m on 14/06/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ML/MLApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLApp.swift
+//  ML
+//
+//  Created by m on 14/06/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct MLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
